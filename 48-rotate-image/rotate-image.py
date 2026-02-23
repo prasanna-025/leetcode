ac1cab8b2a0b40951a7911eb0ab7d2b5e8906fd2
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-02-23 17:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (58.63%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotate(self, matrix):
+        for  i in range(len(matrix)):
+            for j in range(i,len(matrix)):
+                matrix[j][i],matrix[i][j]=matrix[i][j],matrix[j][i]
+            
+        for i in range(len(matrix)):
+            matrix[i].reverse()
+
+        return matrix
+
+  
