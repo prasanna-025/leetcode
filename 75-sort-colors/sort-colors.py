fbe56ac13d5f516a2ee5b2f53499ac349ab87371
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-05-28 15:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (90.33%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,5 +1,19 @@
 class Solution(object):
     def sortColors(self, nums):
-        nums.sort()
+
+        z=[]
+        o=[]
+        s=[]
+
+        for i in nums:
+            if i==0:
+                z.append(i)
+            elif i==1:
+                o.append(i)
+            else:
+                s.append(i)
+        result= z+o+s
+
+        for i in range(len(nums)):
+            nums[i]=result[i]
         return nums
-        
