f9518e45447c655979f8964883e94c2f14085e20
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-05-28 15:02:00 +0530

    Time: 2478 ms (11.20%) | Memory: 13.4 MB (12.62%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def missingNumber(self, nums):
-        n=len(nums)
-        freq={}
-
-        for  i in nums:
-            freq[i]=1
-        for i in range(n+1):
-            if i not in freq:
+       
+        for i in range(len(nums)+1):
+            if i not in nums:
                 return i
         
