cdfa1ab8423e3eea8578e68565c7d4b3ab59fb98
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-06-10 16:32:08 +0530

    Time: 23 ms (78.55%) | Memory: 37.2 MB (7.36%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,12 +1,11 @@
-class Solution(object):
-    def findDuplicate(self, nums):
-        freq={}
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        k={}
 
         for i in nums:
-            if i not in freq:
-                freq[i]=1
+            if i not in k:
+                k[i]=1
             else:
-                freq[i]+=1
-
-            if freq[i]>=2:
-                return i        
+                k[i]+=1
+            if k[i]>=2:
+                return i
