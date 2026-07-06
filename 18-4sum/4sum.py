2bede65c5ce7fb5a24710d212abc89f1c0820597
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-07-06 17:51:09 +0530

    Time: 572 ms (14.64%) | Memory: 19.4 MB (21.72%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,28 +1,33 @@
-class Solution(object):
-    def fourSum(self, nums, target):
-        k=[]
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
+        result =[]
+        if  len(nums)<4:
+            return []
         for i in range(len(nums)-3):
-            if i>0 and  nums[i]==nums[i-1]:
+            if nums[i]==nums[i-1]  and i>0:
                 continue
             for j in range(i+1,len(nums)-2):
-                if j>i+1 and nums[j]==nums[j-1]:
+                if nums[j]==nums[j-1] and j>i+1:
                     continue
                 l=j+1
                 r=len(nums)-1
                 while l<r:
                     s=nums[i]+nums[j]+nums[l]+nums[r]
-                    if s== target:
-                        k.append([nums[i],nums[j],nums[l],nums[r]])
-
-                        while l<r and nums[l]==nums[l+1]:
-                            l=l+1
-                        while l<r and nums[r]==nums[r-1]:
-                            r=r-1
+                    if s==target:
+                        result.append([nums[i],nums[j],nums[l],nums[r]])
                         l+=1
                         r-=1
-                    elif s>target:
-                        r-=1
-                    else:
+                        while l<r and nums[l]==nums[l-1]:
+                            l=l+1
+                        while l<r and nums[r]==nums[r+1]:
+                            r=r-1
+                    elif s<target:
                         l=l+1
-        return k
+                        while l<r and nums[l]==nums[l-1]:
+                            l=l+1
+                    elif s>target:
+                        r=r-1
+                        while l<r and nums[r]==nums[r+1]:
+                            r=r-1
+        return result
