5b3571fd003753e92fc023e4f7ab45e60ff93caa
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-07-07 17:07:29 +0530

    Time: 21 ms (11.48%) | Memory: 12.6 MB (28.65%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,13 +1,28 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
         count=[]
-        i=0
-        while i<len(nums1):
-            target=nums1[i]
-            l=0
-            while l<len(nums2):
-                if target==nums2[l] and target not in count:
-                    count.append(target)
-                l+=1
-            i+=1
-        return count
+        u=[]
+        freq={}
+
+        for i in nums1:
+            if i  in nums2:
+                count.append(i)
+
+
+
+        for i in count:
+            if i not in freq:
+                freq[i]=1
+            else:
+                freq[i]+=1
+
+        for i in freq:
+            if freq[i]>=1:
+                u.append(i)
+
+
+            
+  
+
+
+        return u
