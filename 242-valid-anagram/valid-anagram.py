c977d48b1a0207a1204c6a18e98fd9542112c674
prasanna v <149682301+prasanna-025@users.noreply.github.com>
2026-07-21 15:05:06 +0530

    Time: 19 ms (16.33%) | Memory: 20.3 MB (9.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,27 +1,8 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
 
-        if len(s)!=len(t):
-            return False
 
-        freq={}
-
-        for i in s:
-            if i in freq:
-                freq[i]+=1
-            else:
-                freq[i]=1
-
-        for i in t:
-            if i not in freq:
-                return False
-
-            freq[i]-=1
-
-            if freq[i]<0:
-                return False
-
-        return True
-
+        return sorted(s)==sorted(t)
         
+
         
